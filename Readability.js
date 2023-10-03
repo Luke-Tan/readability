e2472bd4f84e958be639181eddff029fe8cec873
Luke Tan <luketan1998@gmail.com>
2023-10-03 15:44:31 +0800

    feat: add news to positive regexps

--- Readability.js
+++ Readability.js
@@ -139,7 +139,7 @@ Readability.prototype = {
     okMaybeItsACandidate: /and|article|body|column|content|main|shadow/i,
 
     positive:
-      /article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story/i,
+      /article|body|content|entry|hentry|h-entry|main|page|pagination|post|text|blog|story|news/i,
     negative:
       /-ad-|hidden|^hid$| hid$| hid |^hid |banner|combx|comment|com-|contact|foot|footer|footnote|gdpr|masthead|media|meta|outbrain|promo|related|scroll|share|shoutbox|sidebar|skyscraper|sponsor|shopping|tags|tool|widget/i,
     extraneous:
